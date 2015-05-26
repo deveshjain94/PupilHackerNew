8ebed271640e67ed70b1edd1b1686dac84324130
Devesh <deveshjain94@gmail.com>
2015-05-26 10:23:56 +0530

    Added CMake files and updated README

--- install_lib.sh
+++ install_lib.sh
@@ -1,6 +1,6 @@
 #run by typing sh install_lib.sh
-sudo apt-get install libboost-dev &&
-sudo apt-get install gnuplot &&
-sudo apt-get install libhpdf-dev
+sudo apt-get install -y libboost-dev &&
+sudo apt-get install -y gnuplot &&
+sudo apt-get install -y libhpdf-dev
 mkdir Reports
 mkdir Videos
